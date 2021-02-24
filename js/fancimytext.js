0307d81771faf63cfc9201ec11ac5cc2048d55ab
nguyen-cyber <72995742+nguyen-cyber@users.noreply.github.com>
2021-02-23 17:07:42 -0800

    Add files via upload

---
+++ js/fancimytext.js
@@ -0,0 +1,36 @@
+// CSC 196W 
+// Minh Nguyen
+// JavaScript code for fancifymytext
+
+//alert("Hello, world!");
+
+function makeTextBigger() {
+    document.getElementById("textInput").style.fontSize = "24pt";
+}
+
+function makeTextBoring() {
+    if (document.getElementById("boring").checked == true) {    
+        document.getElementById("textInput").style.fontSize = "10pt";
+        document.getElementById("textInput").style.color = "black";
+        document.getElementById("textInput").style.textDecoration = "none";
+        document.getElementById("boring").checked = false;
+    }
+}
+
+function makeTextFancy() {
+    if (document.getElementById("fancy").checked == true) {
+        document.getElementById("textInput").style.color = "blue";
+        document.getElementById("textInput").style.textDecoration = "underline";
+        document.getElementById("fancy").checked = false;
+    }
+}
+
+function moo() {
+    var mooElem = document.getElementById("textInput");
+    mooElem.value = mooElem.value.toUpperCase();
+    var parts = mooElem.value.split(".");
+    for (var i = 0; i< parts.length; i++){
+        parts[i] = parts[i] + "Moo";
+    }
+    mooElem.value = parts.join(".");
+}
